26dbd61a38dd0ff273cbbdfdbe83d0f914088e08
Darrell Cornelius R <darrellcornelius37@gmail.com>
2025-06-12 22:39:12 +0700

    Initial Commit

---
+++ SoonToBeNamed/SoonToBeNamedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoonToBeNamedApp.swift
+//  SoonToBeNamed
+//
+//  Created by Darrell Cornelius Rivaldo on 12/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SoonToBeNamedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SoonToBeNamed/Supabase.swift
@@ -0,0 +1,9 @@
+//
+//  Supabase.swift
+//  SoonToBeNamed
+//
+//  Created by Darrell Cornelius Rivaldo on 12/06/25.
+//
+
+import Foundation
+
