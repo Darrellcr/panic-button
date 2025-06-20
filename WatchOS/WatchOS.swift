f637e4337bd441b9c17d6d5c63beddf7c04f5e3e
Calvin Christian Tjong <calvinchristiant3@gmail.com>
2025-06-20 15:21:57 +0700

    guardianview

--- WatchOS/WatchOS.swift
+++ WatchOS/WatchOS.swift
@@ -35,7 +35,37 @@ struct WatchOSEntryView : View {
     var entry: Provider.Entry
     
     var body: some View {
-        WatchOSWidgetView()
+        ZStack {
+            Capsule()
+                .fill(.color)
+            HStack{
+                Image("logo_transparent")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 50, height: 50)
+                    .offset(x: 0, y: 2)
+                Text("SOS")
+                    .font(.title)
+                    .bold()
+//                    .foregroundStyle(.white)
+                    .offset(x: -10)
+            }
+//            .padding(.horizontal,10)
+           
+        }
+//        ZStack{
+//            Capsule()
+//                .fill(.red)
+//            HStack{
+//                Image("logo_transparent")
+//                    .resizable()
+//                    .scaledToFit()
+//                    .frame(width: 40)
+//                Text("Dua")
+//            }
+//        }
+        .widgetURL(URL(string: "myapp://sos"))
+        .containerBackground(.fill.tertiary, for: .widget)
     }
 }
 
